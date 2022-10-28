6b4af7adc767c94f5c52d179af16df81034c2ce2
Olga Matsulevich <flintrok@ya.ru>
2022-10-28 15:39:50 +0300

    Task4 added

---
+++ Homework1/Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿int number1 = 25;
+int number2 = 0;
+int number3 = 5;
+int max = 0;
+if (number1 > number2)
+{
+    max = number1;
+}
+else
+{
+    max = number2;
+}
+if (max < number3)
+{
+max = number3;
+};
+Console.WriteLine("max = "+ max);
