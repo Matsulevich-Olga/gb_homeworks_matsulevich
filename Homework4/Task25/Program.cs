2c41cbad84bbd0f4426f92735998555f9b7d137e
Olga Matsulevich <flintrok@ya.ru>
2022-11-02 23:21:35 +0300

    Task25 Added

--- Homework4/Task25/Program.cs
+++ Homework4/Task25/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int power(int A, int B)
+{
+    int power = A;
+    for (int i = 2; i <= B; i++)
+    {
+        power = power * A;
+    }
+    return power;
+}
+
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите cтепень: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(power(A, B));
