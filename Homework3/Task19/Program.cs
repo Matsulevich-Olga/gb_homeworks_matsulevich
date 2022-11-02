f5d13a3920aa6e4d2c0844ccbf1eb7e39c6d5560
Olga Matsulevich <flintrok@ya.ru>
2022-11-02 18:19:31 +0300

    Task19 Added

---
+++ Homework3/Task19/Program.cs
@@ -0,0 +1,22 @@
+﻿//принимаем на вход пятизначное число
+int number = 0;
+while (number < 10000 || number > 99999)
+{
+    Console.Write("Введите пятизначное число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+    if (number < 10000 || number > 99999)
+    {
+        Console.WriteLine("Число не пятизначное");
+    }
+}
+
+//cравниваем первую и поледнюю цифру, вторую и четвертую в числе
+if (number / 10000 == number % 10)
+{
+    if (number / 1000 % 10 == number % 100 / 10)
+    {
+        Console.WriteLine("Да");
+    }
+    else { Console.WriteLine("Нет"); }
+}
+else { Console.WriteLine("Нет"); }
