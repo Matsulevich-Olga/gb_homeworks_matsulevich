d8efb019e2af64dcbf3332499f585107392c89b5
Olga Matsulevich <flintrok@ya.ru>
2022-11-03 00:03:25 +0300

    Task29 Added

---
+++ Homework4/Task29/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] randomArray(int n)
+{
+    int[] array = new int[n];
+    Random rand = new Random();
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = rand.Next(0, 100);
+        Console.Write(array[i] + " ");
+    }
+    return array;
+}
+
+Console.Write("Введите число: ");
+int[] randArray = randomArray(Convert.ToInt32(Console.ReadLine()));
