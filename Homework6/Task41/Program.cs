c15b065e45d8fed06ce5210a0f33743ca889e120
Olga Matsulevich <flintrok@ya.ru>
2022-11-06 17:16:18 +0300

    Task41 Added

--- Homework6/Task41/Program.cs
+++ Homework6/Task41/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Преобразуем строку в массив строк.
+
+string[] stringToArray(string s)
+{
+    string[] stringArray = s.Split(" ");
+    return stringArray;
+}
+
+
+//Преобразуем массив сторок в массив целых чисел.
+
+int[] strToIntArray(string[] stringArray)
+{
+    int[] intArray = new int[stringArray.Length];
+    for (int i = 0; i < intArray.Length; i++)
+    {
+        intArray[i] = Convert.ToInt32(stringArray[i]);
+    }
+    return intArray;
+}
+
+//Считаем количество положительных чисел.
+
+int positiveCount(int[] intArray)
+{
+    int count = 0;
+    for (int i = 0; i < intArray.Length; i++)
+    {
+        if (intArray[i] > 0)
+        {
+            count++;
+        }
+
+    }
+    return count;
+}
+
+//Решение.
+
+Console.Write("Введите числа: ");
+string str = Console.ReadLine();
+string[] strArray = stringToArray(str);
+int[] intArray = strToIntArray(strArray);
+Console.WriteLine(positiveCount(intArray));
+
+
+
+
