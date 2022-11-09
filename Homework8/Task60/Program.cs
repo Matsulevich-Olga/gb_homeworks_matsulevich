c6221edd065e05b89572ede148ae3b1950b82a9c
Olga Matsulevich <flintrok@ya.ru>
2022-11-10 00:41:00 +0300

    Task60 Added

---
+++ Homework8/Task60/Program.cs
@@ -0,0 +1,40 @@
+﻿//Cоздание трехмерного массива.
+
+int[,,] Create3DArray(int n, int m, int l)
+{
+    int[,,] array = new int[n, m, l];
+    return array;
+}
+
+// Заполнение трехмерного массива.
+
+int[,,] fill3DArray(int[,,] array, int min, int max)
+{
+    Random rand = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = rand.Next(min, max);
+            }
+    return array;
+}
+
+//Печать трехмереного массива.
+
+void print3DArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++, Console.WriteLine(""))
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[j, k, i]} ({j},{k},{i}) \t");
+            }
+}
+
+//Решение.
+
+int[,,] array = Create3DArray(2, 2, 2);
+array = fill3DArray(array, 10, 99);
+print3DArray(array);
+
