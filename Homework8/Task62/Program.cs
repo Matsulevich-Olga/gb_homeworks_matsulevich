d30424130589ca719db34f1eb4838ee88ef4e035
Olga Matsulevich <flintrok@ya.ru>
2022-11-10 14:07:14 +0300

    Task62 Added

---
+++ Homework8/Task62/Program.cs
@@ -0,0 +1,46 @@
+﻿// Спиральное заполнение массива. 
+
+int[,] FillSpiral(int n)
+{
+    int[,] array = new int[n, n];
+
+    int pos = 0;
+    int count = n;
+    int value = -n;
+    int sum = -1;
+
+    while (count > 0)
+    {
+        value = -1 * value / n;
+        for (int i = 0; i < count; i++)
+        {
+            sum += value;
+            array[sum / n, sum % n] = pos++;
+        }
+        value *= n;
+        count--;
+        for (int i = 0; i < count; i++)
+        {
+            sum += value;
+            array[sum / n, sum % n] = pos++;
+        }
+    }
+    return array;
+}
+
+
+//Печать массива
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++, Console.WriteLine(""))
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+}
+
+//Решение.
+
+int[,] array = FillSpiral(4);
+PrintArray(array);
