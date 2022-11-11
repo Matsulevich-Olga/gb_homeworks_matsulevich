9e594243ba8ce90bd8325acd2e7ee86bcc190b9e
Olga Matsulevich <flintrok@ya.ru>
2022-11-11 18:13:40 +0300

    Task66 Added

---
+++ Homework9/Task66/Program.cs
@@ -0,0 +1,20 @@
+﻿//Метод рекурсивного подсчета суммы чисел от M до N.
+
+using static System.Console;
+
+int RecursiveSum(int m, int n)
+{
+    if (m == n)
+    {
+        return m;
+    }
+    else
+    {
+        return n + RecursiveSum(m, n - 1);
+    }
+}
+
+//Решение.
+
+WriteLine(RecursiveSum(4, 8));
+
