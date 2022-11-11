b4e1fa9a585b2412af7db781d109967cbfcc4d31
Olga Matsulevich <flintrok@ya.ru>
2022-11-11 17:52:25 +0300

    Task64 Added

---
+++ Homework9/Task64/Program.cs
@@ -0,0 +1,20 @@
+﻿//Метод рекурсивного вывода чисел от N до 1.
+
+using static System.Console;
+
+string RecursiveNumbers(int n)
+{
+    if (n == 1)
+    {
+        return "1";
+    }
+    else
+    {
+        return n + ", " + RecursiveNumbers(n - 1);
+    }
+}
+
+//Решение.
+
+WriteLine(RecursiveNumbers(8));
+
